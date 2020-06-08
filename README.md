fdbf366bd0a19329531f04d63dffe42f67a850c9
dewoolkaridhish4 <65116701+dewoolkaridhish4@users.noreply.github.com>
2020-06-08 21:06:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
-# C24 Project 
-
-
-Output lnk
-
-
-https://dewoolkaridhish4.github.io/C24-Project/.
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
--- sketch.js
+++ sketch.js
@@ -3,21 +3,20 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(800,700);
 
   engine = Engine.create();
 	world = engine.world;
 
   ground1= new Ground(370,695,870,10);
-	ball1=new Ball(55,576,10,10);
-	box1=new Box(657,640,20,100);
-	box2=new Box(754,640,20,100);
+	paper=new Paper(55,576);
+	box1=new Box(647,640,20,100);
+	box2=new Box(766,640,20,100);
 	box3=new Box(707,680,100,20);
 	
 	Engine.run(engine);
   
 }
-
 
 function draw() {
   rectMode(CENTER);
@@ -28,20 +27,18 @@ function draw() {
   text("x:"+mouseX,50,50);
 
   ground1.display();
-  ball1.display();
+  paper.display();
   box1.display();
   box2.display();
   box3.display();
   
-  keyPressed();
-
   drawSprites();
  
 }
 
 function keyPressed(){
 	if(keyCode === UP_ARROW){
-		Matter.Body.applyForce(ballObject.body,ballObject.body.position,{x:85,y:-85});
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:39,y:-39});
 	}
 }
 
